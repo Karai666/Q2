d41a93563b714633fbd256d8da6673635aa0c85e
Karai666 <161113407+Karai666@users.noreply.github.com>
2024-05-04 21:19:31 +0330

    Create Q2_Password.py

---
+++ Q2/Q2_Password.py
@@ -0,0 +1,138 @@
+
+#Authors: sheida samadi , baran farrokhian , parsa bastami
+
+#creating password class
+class Creating_Password:
+    def __init__ (self ,Pasword):
+        self.Pasword = Pasword
+
+# creating password as object
+Pass1  = Creating_Password('RCvNz1rWRp1Bm87')
+Pass2  = Creating_Password('BQq8W80YT7q0P6')
+Pass3  = Creating_Password('PPTWgg3vGRLkJP4b')
+Pass4  = Creating_Password('9MCys9gQPstLHlryp')
+Pass5  = Creating_Password('SDJJwybYmwGvH6ZQHO')
+Pass6  = Creating_Password('gc41dDmDk5Yfxr1nGC')
+Pass7  = Creating_Password('xTNY54K5tY7YQ')
+Pass8  = Creating_Password('KQT08tj4C12CH6')
+Pass9  = Creating_Password('mCrjrB4vPkn0x0F')
+Pass10 = Creating_Password('Gh3WJKhYgydvGTBh')
+
+symbols   = ['@' , '#']
+vowels    = ['a', 'i', 'u', 'e', 'o']
+letters   = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+# checking the password 1 conditions 
+if len(Pass1.Pasword) not in range (12 , 20):
+           print('Password Should Be Between 12 & 19 Characters. ')
+if not (char.vowels for char in Pass1.Pasword):
+            print ('Password Shouldent Have Vowels Letters Like ( a, i, u, e, o ). ')
+if not any(char.isupper() for char in Pass1.Pasword):
+            print('Pasword Should Have Capital Letter As Well. ')
+if not any (char.islower() for char in Pass1.Pasword):
+            print('Pasword Should Have Small Letter As Well. ')
+print('Paasword 1 :',Pass1.Pasword)
+
+
+# checking the password 2 conditions 
+if len(Pass2.Pasword) not in range (12 , 20):
+           print('Password Should Be Between 12 & 19 Characters. ')
+if not (char.vowels for char in Pass2.Pasword):
+            print ('Password Shouldent Have Vowels Letters Like ( a, i, u, e, o ). ')
+if not any(char.isupper() for char in Pass2.Pasword):
+            print('Pasword Should Have Capital Letter As Well. ')
+if not any (char.islower() for char in Pass2.Pasword):
+            print('Pasword Should Have Small Letter As Well. ')
+print('Paasword 2 :',Pass2.Pasword)
+
+# checking the password 3 conditions 
+if len(Pass3.Pasword) not in range (12 , 20):
+           print('Password Should Be Between 12 & 19 Characters. ')
+if not (char.vowels for char in Pass3.Pasword):
+            print ('Password Shouldent Have Vowels Letters Like ( a, i, u, e, o ). ')
+if not any(char.isupper() for char in Pass3.Pasword):
+            print('Pasword Should Have Capital Letter As Well. ')
+if not any (char.islower() for char in Pass3.Pasword):
+            print('Pasword Should Have Small Letter As Well. ')
+print('Paasword 3 :',Pass3.Pasword)
+
+# checking the password 4 conditions 
+if len(Pass4.Pasword) not in range (12 , 20):
+           print('Password Should Be Between 12 & 19 Characters. ')
+if not (char.vowels for char in Pass4.Pasword):
+            print ('Password Shouldent Have Vowels Letters Like ( a, i, u, e, o ). ')
+if not any(char.isupper() for char in Pass4.Pasword):
+            print('Pasword Should Have Capital Letter As Well. ')
+if not any (char.islower() for char in Pass4.Pasword):
+            print('Pasword Should Have Small Letter As Well. ')
+print('Paasword 4 :',Pass4.Pasword)
+
+
+# checking the password 5 conditions 
+if len(Pass5.Pasword) not in range (12 , 20):
+           print('Password Should Be Between 12 & 19 Characters. ')
+if not (char.vowels for char in Pass5.Pasword):
+            print ('Password Shouldent Have Vowels Letters Like ( a, i, u, e, o ). ')
+if not any(char.isupper() for char in Pass5.Pasword):
+            print('Pasword Should Have Capital Letter As Well. ')
+if not any (char.islower() for char in Pass5.Pasword):
+            print('Pasword Should Have Small Letter As Well. ')
+print('Paasword 5 :',Pass5.Pasword)
+
+# checking the password 6 conditions 
+if len(Pass6.Pasword) not in range (12 , 20):
+           print('Password Should Be Between 12 & 19 Characters. ')
+if not (char.vowels for char in Pass6.Pasword):
+            print ('Password Shouldent Have Vowels Letters Like ( a, i, u, e, o ). ')
+if not any(char.isupper() for char in Pass6.Pasword):
+            print('Pasword Should Have Capital Letter As Well. ')
+if not any (char.islower() for char in Pass6.Pasword):
+            print('Pasword Should Have Small Letter As Well. ')
+print('Paasword 6 :',Pass6.Pasword)
+
+
+# checking the password 7 conditions 
+if len(Pass7.Pasword) not in range (12 , 20):
+           print('Password Should Be Between 12 & 19 Characters. ')
+if not (char.vowels for char in Pass7.Pasword):
+            print ('Password Shouldent Have Vowels Letters Like ( a, i, u, e, o ). ')
+if not any(char.isupper() for char in Pass7.Pasword):
+            print('Pasword Should Have Capital Letter As Well. ')
+if not any (char.islower() for char in Pass7.Pasword):
+            print('Pasword Should Have Small Letter As Well. ')
+print('Paasword 7 :',Pass7.Pasword)
+
+
+# checking the password 8 conditions 
+if len(Pass8.Pasword) not in range (12 , 20):
+           print('Password Should Be Between 12 & 19 Characters. ')
+if not (char.vowels for char in Pass8.Pasword):
+            print ('Password Shouldent Have Vowels Letters Like ( a, i, u, e, o ). ')
+if not any(char.isupper() for char in Pass8.Pasword):
+            print('Pasword Should Have Capital Letter As Well. ')
+if not any (char.islower() for char in Pass8.Pasword):
+            print('Pasword Should Have Small Letter As Well. ')
+print('Paasword 8 :',Pass8.Pasword)
+
+# checking the password 9 conditions 
+if len(Pass9.Pasword) not in range (12 , 20):
+           print('Password Should Be Between 12 & 19 Characters. ')
+if not (char.vowels for char in Pass9.Pasword):
+            print ('Password Shouldent Have Vowels Letters Like ( a, i, u, e, o ). ')
+if not any(char.isupper() for char in Pass9.Pasword):
+            print('Pasword Should Have Capital Letter As Well. ')
+if not any (char.islower() for char in Pass9.Pasword):
+            print('Pasword Should Have Small Letter As Well. ')
+print('Paasword 9 :',Pass9.Pasword)
+
+
+# checking the password 10 conditions 
+if len(Pass10.Pasword) not in range (12 , 20):
+           print('Password Should Be Between 12 & 19 Characters. ')
+if not (char.vowels for char in Pass10.Pasword):
+            print ('Password Shouldent Have Vowels Letters Like ( a, i, u, e, o ). ')
+if not any(char.isupper() for char in Pass10.Pasword):
+            print('Pasword Should Have Capital Letter As Well. ')
+if not any (char.islower() for char in Pass10.Pasword):
+            print('Pasword Should Have Small Letter As Well. ')
+print('Paasword 10 :',Pass10.Pasword)
